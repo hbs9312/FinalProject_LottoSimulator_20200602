3f982edf6f268522922a8212903e9a57f81d79a7
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-06-02 10:16:44 +0900

    당첨번호 6개 뽑기  - 추첨 전에 배열을 모두 0으로 초기화

--- app/src/main/java/kr/co/tjoeun/finalproject_lottosimulator_20200602/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/finalproject_lottosimulator_20200602/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.Arrays;
 
 import kr.co.tjoeun.finalproject_lottosimulator_20200602.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
 
     ActivityMainBinding binding;
+
+    int[] winLottoNumArr = new int[6];
+    int bonusNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +29,57 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvent() {
 
+        binding.buyOneLottoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeLottoWinNum();
+            }
+        });
+
     }
 
     @Override
     public void setValues() {
 
     }
+
+    void makeLottoWinNum() {
+
+        for (int i=0; i < winLottoNumArr.length; i++) {
+            winLottoNumArr[i] = 0;
+        }
+
+        for(int i=0; i < winLottoNumArr.length; i++) {
+
+            while(true) {
+
+                int randomNum = (int) (Math.random()*45 + 1);
+
+                boolean isDuplOk = true;
+                for (int num : winLottoNumArr) {
+
+                    if (num == randomNum) {
+                        isDuplOk = false;
+                        break;
+                    }
+
+                }
+
+                if(isDuplOk){
+                    winLottoNumArr[i] = randomNum;
+                    break;
+                }
+
+            }
+
+        }
+
+        Arrays.sort(winLottoNumArr);
+
+        for(int winNum : winLottoNumArr) {
+            Log.d("당첨번호", winNum+"");
+        }
+
+    }
+
 }
